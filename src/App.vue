f16197a1fb20723aa2042dd29de06b660c2c1b65
GUO <freedomxuanyuan@163.com>
2024-09-02 18:25:03 +0800

    This is a demo

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,27 @@
 <!--  <ThreeView />-->
   <div>{{ state.demo }}</div>
   <div class="ceshi">
-    <div class="title" v-for="(item, index) in state.demoTitle" @click="chaneColor(item)" :class="{active : item.title === state.changeColorNum}">{{ item.title }}</div>
+    <div class="title" style="user-select: none" v-for="(item, index) in state.demoTitle" @click="chaneColor(item)" :class="{active : item.title === state.changeColorNum}">{{ item.title }}</div>
   </div>
   <hr>
-  <div>This is a demo</div>
+  <div>
+    <div @click="changeTrue" style="cursor: pointer;">
+      change
+    </div>
+    {{state.demoTrue ? "yes": "no"}}<br>
+    123<br>
+    {{formatGetDate()}}
+  </div>
+  <div class="switchBox">
+    <div class="switch" @click="changeSwitchTrue" :class="{active : state.switchTrue==true}">
+
+    </div>
+  </div>
+  <div  v-html="state.demoHtml"></div>
 </template>
 <script setup lang="ts">
-import * as THREE from 'three';
-import ThreeView from './views/ThreeView.vue'
 import {reactive} from "vue";
+
 const state = reactive<any>({
   chartsStatus: '',
   dec: {} as any,
@@ -29,10 +41,21 @@ const state = reactive<any>({
     {title: 'Demo5'},
   ],
   changeColorNum: "Demo1",
+  demoHtml: '<h1 id="demoHtml">This is a Test!</h1>',
+  demoTrue: false,
+  switchTrue: true,
 })
+const changeTrue = () => {
+  state.demoTrue = state.demoTrue != true;
+}
+const changeSwitchTrue = () => {
+  state.switchTrue = state.switchTrue != true;
+}
 const chaneColor = (item: any) => { //更改颜色
   state.changeColorNum = item.title;
-  // console.log(item, state.changeColorNum);
+}
+const formatGetDate = () => {
+  return new Date();
 }
 </script>
 <!--<script setup lang="ts">-->
@@ -57,7 +80,31 @@ const chaneColor = (item: any) => { //更改颜色
     cursor: pointer;
   }
 }
-
+.switchBox {
+  width: 70px;
+  height: 30px;
+  position: relative;
+  border: 6px solid #ccc;
+  border-radius: 200px;
+  .switch {
+    width: 30px;
+    height: 30px;
+    box-sizing: border-box;
+    border: 6px solid #000;
+    border-radius: 50%;
+    position: absolute;
+    background: #000;
+    cursor: pointer;
+    //left: 0;
+    right: 0;
+    transition: .3s;
+    &.active {
+      //position: absolute;
+      right: 40px;
+      background: rgba(0, 0, 0, 0);
+    }
+  }
+}
 nav {
   padding: 30px;
 }
